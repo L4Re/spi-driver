cdb1410e58ea5ad0cbba8ab53870ede28914598d
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2025-10-17 16:18:04 +0200

    document client facing SPI device interface
    Change-Id: I327dd0d66d2ccdf40869817bfc055f43a9d952da
    Signed-off-by: Philipp Eppelt <philipp.eppelt@kernkonzept.com>

--- include/spi_device_if.h
+++ include/spi_device_if.h
@@ -14,16 +14,48 @@
 
 struct Spi_device_ops : L4::Kobject_t<Spi_device_ops, L4::Kobject, L4::PROTO_ANY>
 {
+  /**
+   * Transfer data to and from the currently selected device.
+   *
+   * \param wbuf  Data to transfer to the device.
+   * \param rbuf  Buffer for data to read from the device.
+   *
+   * \return  L4_EOK or error code.
+   */
   L4_INLINE_RPC(long, transfer,
                 (L4::Ipc::Array<l4_uint8_t const> wbuf,
                  L4::Ipc::Array<l4_uint8_t> &rbuf));
 
+  /**
+   * Write data to a device and then read from the same device `len` bytes.
+   *
+   * \param data  Data buffer to write to the device.
+   * \param len   Amount of data to read from the device.
+   * \param buf   Buffer for the read data.
+   *
+   * \return  L4_EOK or error code.
+   */
   L4_INLINE_RPC(long, write_read,
                 (L4::Ipc::Array<l4_uint8_t const> data, unsigned char len,
                  L4::Ipc::Array<l4_uint8_t> &buf));
 
+  /**
+   * Write data to the currently selected device.
+   *
+   * \param buf  Data to write to the device.
+   *
+   * \return  L4_EOK or error code.
+   */
   L4_INLINE_RPC(long, write, (L4::Ipc::Array<l4_uint8_t const> buf));
 
+  /**
+   * Read `len` number of bytes from the currently selected device.
+   *
+   * \param len  Number of bytes to read.
+   * \param buf  Buffer to read the data into.
+   *
+   * \return  L4_EOK or error code.
+   */
   L4_INLINE_RPC(long, read,
                 (unsigned char len, L4::Ipc::Array<l4_uint8_t> &buf));
 
